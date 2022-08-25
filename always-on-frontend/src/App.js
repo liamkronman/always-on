33e4739466303d82d4afcba6f7a1b3e63b07f1a9
frodakcin <huangsy222@gmail.com>
2022-08-25 00:19:07 -0400

    testing peerjs connector

--- always-on-frontend/src/App.js
+++ always-on-frontend/src/App.js
@@ -1,67 +1,154 @@
-// import logo from './logo.svg';
-import './App.css';
-import { Peer } from 'peerjs';
-import { useRef, useEffect } from 'react';
+import { useEffect, useRef, useState } from "react";
+import Peer from "peerjs";
+import "./App.css";
 
 function App() {
-  const peer = new Peer('liam-always-on');
+	const [peerId, setPeerId] = useState("");
+	const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+	const remoteVideoRef = useRef(null);
+	const currentUserVideoRef = useRef(null);
+	const peerInstance = useRef(null);
 
-  const conn = peer.connect("siyong-always-on");
-  conn.on("open", () => {
-    conn.send("hi!");
-  });
+	useEffect(() => {
+		const peer = new Peer();
 
-  peer.on("connection", (conn) => {
-    conn.on("data", (data) => {
-      console.log(data);
-    })
-    conn.on("open", () => {
-      conn.send("hello!");
-    })
-  })
+		peer.on("open", (id) => {
+			setPeerId(id);
+		});
 
-  const videoRef = useRef();
+		peer.on("call", (call) => {
+			var getUserMedia =
+				navigator.getUserMedia ||
+				navigator.webkitGetUserMedia ||
+				navigator.mozGetUserMedia;
 
-  const getStream = async (screenId) => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-          mandatory: {
-            chromeMediaSource: 'desktop',
-            chromeMediaSourceId: screenId,
-          }
-        }
-      })
+			getUserMedia({ video: true, audio: true }, (mediaStream) => {
+				currentUserVideoRef.current.srcObject = mediaStream;
+				currentUserVideoRef.current.play();
+				call.answer(mediaStream);
+				call.on("stream", function (remoteStream) {
+					remoteVideoRef.current.srcObject = remoteStream;
+					remoteVideoRef.current.play();
+				});
+			});
+		});
 
-      handleStream(stream)
-    } catch(e) {
-      console.log(e)
-    }
-  }
+		peerInstance.current = peer;
+	}, []);
 
-  const handleStream = (stream) => {
-    let { width, height } = stream.getVideoTracks()[0].getSettings();
+	const call = (remotePeerId) => {
+		var getUserMedia =
+			navigator.getUserMedia ||
+			navigator.webkitGetUserMedia ||
+			navigator.mozGetUserMedia;
 
-    // window.electronAPI.setSize({ width, height });
+		getUserMedia({ video: true, audio: true }, (mediaStream) => {
+			currentUserVideoRef.current.srcObject = mediaStream;
+			currentUserVideoRef.current.play();
 
-    videoRef.current.srcObject = stream;
-    videoRef.current.onloadedmetadata = (e) => videoRef.current.play()
-  }
+			const call = peerInstance.current.call(remotePeerId, mediaStream);
 
-  window.electronAPI.getScreenId((event, screenId) => {
-    console.log('Renderer...', screenId);
-    getStream(screenId);
-  });
+			call.on("stream", (remoteStream) => {
+				remoteVideoRef.current.srcObject = remoteStream;
+				remoteVideoRef.current.play();
+			});
+		});
+	};
 
-  return (
-    <div className="App">
-      <>
-        <span>800 x 600</span>
-        <video ref={videoRef} className="video"></video>
-      </>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Current user id is {peerId}</h1>
+			<input
+				type="text"
+				value={remotePeerIdValue}
+				onChange={(e) => setRemotePeerIdValue(e.target.value)}
+			/>
+			<button onClick={() => call(remotePeerIdValue)}>Call</button>
+			<div>
+				<video ref={currentUserVideoRef} />
+			</div>
+			<div>
+				<video ref={remoteVideoRef} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
+// // import logo from './logo.svg';
+// import "./App.css";
+// import { Peer } from "peerjs";
+// import { useRef, useEffect } from "react";
+
+// function App() {
+// 	const peer = new Peer();
+// 	peer.on("open", (id) => {
+// 		console.log("Opened, my ID is", id);
+// 	});
+// 	console.log(peer);
+// 	peer.on("error", (message) => console.log("Errored out", message));
+
+// 	const conn = peer.connect("d25b0359-641d-42f0-abcb-2499cb35ffa7");
+// 	//console.log("Is conn open?", conn.open());
+// 	conn.on("open", () => {
+// 		console.log("conn Connected");
+// 		conn.send("Hi!");
+// 	});
+// 	peer.on("error", (message) => console.log("Errored out", message));
+
+// 	peer.on("connection", (conn) => {
+// 		conn.on("data", (data) => {
+// 			console.log(data);
+// 		});
+// 		conn.on("open", () => {
+// 			console.log("Opened 2?");
+// 			conn.send("Hello!");
+// 		});
+// 	});
+
+// 	const videoRef = useRef();
+
+// 	const getStream = async (screenId) => {
+// 		try {
+// 			const stream = await navigator.mediaDevices.getUserMedia({
+// 				audio: false,
+// 				video: {
+// 					mandatory: {
+// 						chromeMediaSource: "desktop",
+// 						chromeMediaSourceId: screenId,
+// 					},
+// 				},
+// 			});
+
+// 			handleStream(stream);
+// 		} catch (e) {
+// 			console.log(e);
+// 		}
+// 	};
+
+// 	const handleStream = (stream) => {
+// 		let { width, height } = stream.getVideoTracks()[0].getSettings();
+
+// 		// window.electronAPI.setSize({ width, height });
+
+// 		videoRef.current.srcObject = stream;
+// 		videoRef.current.onloadedmetadata = (e) => videoRef.current.play();
+// 	};
+
+// 	window.electronAPI.getScreenId((event, screenId) => {
+// 		console.log("Renderer...", screenId);
+// 		getStream(screenId);
+// 	});
+
+// 	return (
+// 		<div className="App">
+// 			<>
+// 				<span>800 x 600</span>
+// 				<video ref={videoRef} className="video"></video>
+// 			</>
+// 		</div>
+// 	);
+// }
+
+// export default App;

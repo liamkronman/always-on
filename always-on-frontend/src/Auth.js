a4be65673a9f70ce2a704838aaf24ccd3e01f38f
liamkronman <liamkronman@gmail.com>
2022-08-26 20:07:58 -0400

    signup behaves perfectly

--- always-on-frontend/src/Auth.js
+++ always-on-frontend/src/Auth.js
@@ -7,6 +7,7 @@ function Auth({setToken}) {
 	const [signupEmail, setSignupEmail] = useState("");
 	const [signupPassword, setSignupPassword] = useState("");
 	const [signupConfirmPassword, setSignupConfirmPassword] = useState("");
+    const [signupErrorMsg, setSignupErrorMsg] = useState("");
     const signupUsernameRef = useRef();
 
     useEffect(() => {
@@ -22,6 +23,7 @@ function Auth({setToken}) {
                     <div className="login-container">
                         <div></div>
                         <div className="auth-topline">Sign Up</div>
+                        <div className="auth-error-msg">{signupErrorMsg}</div>
                         <div className="auth-input-container">
                             <div className="auth-input-title">Username</div>
                             <input className="auth-input-area" type="text" name="username" placeholder="Set a username." value={signupUsername} ref={signupUsernameRef} onChange={(e) => setSignupUsername(e.target.value)} />
@@ -39,13 +41,42 @@ function Auth({setToken}) {
                             <input className="auth-input-area" type="password" name="confirm-password" placeholder="Re-type that password." value={signupConfirmPassword} onChange={(e) => setSignupConfirmPassword(e.target.value)} />
                         </div>
                         <button className="auth-submit-btn" onClick={() => {
-                            if (signupPassword === signupConfirmPassword) {
-                                axios.post("http://159.223.143.90/api/auth/signin", {
-
-                                })
+                            setSignupErrorMsg("");
+                            if (signupUsername && signupEmail && signupPassword && signupConfirmPassword) {
+                                if (signupPassword === signupConfirmPassword) {
+                                    axios.post("http://159.223.143.90/api/auth/signup", {
+                                        username: signupUsername,
+                                        email: signupEmail,
+                                        password: signupPassword
+                                    })
+                                    .then(resp => {
+                                        axios.post("http://159.223.143.90/api/auth/signin", {
+                                            username: signupUsername,
+                                            email: signupEmail,
+                                            password: signupPassword
+                                        })
+                                        .then(resp => {
+                                            setSignupUsername("");
+                                            setSignupEmail("");
+                                            setSignupPassword("");
+                                            setSignupConfirmPassword("");
+                                            localStorage.setItem("accessToken", resp.data.accessToken);
+                                            setToken(resp.data.accessToken);
+                                        })
+                                    })
+                                    .catch(err => {
+                                        setSignupErrorMsg("Sign up failed. Try again.")
+                                    })
+                                } else {
+                                    setSignupErrorMsg("Passwords do not match.")
+                                }
+                            } else {
+                                setSignupErrorMsg("Please fill out all fields.")
                             }
                         }}>Turn me on</button>
-                        <a className="auth-already-tagline">Already have an account?</a>
+                        <a className="auth-already-tagline" onClick={() => {
+                            setDisplaySignup(false)
+                        }}>Already have an account?</a>
                     </div>
                 </>
                 : <></>

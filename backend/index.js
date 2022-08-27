fbdb52e60937ef6a68d3378c4d37549a2008688d
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-08-28 02:53:06 +0800

    update react express server

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const PORT = 3000;
 
 const app = express();
+app.get('/overlay', (res) => res.sendFile(path.join(__dirname, '../always-on-frontend/build/index.html')));
 app.use(express.static(path.join(__dirname, '../always-on-frontend/build')));
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
